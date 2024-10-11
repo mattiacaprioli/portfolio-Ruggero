d27b56067b86368e09addc95a4698556b802a972
Mattia Caprioli <capriolimattia1994@gmail.com>
2024-10-11 22:39:10 +0200

    refactor script, icon and little things

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,11 +16,11 @@ const Header = () => {
   })
     
   return (
-    <header className={`${bg ? 'bg-tertiary h-20' : 'h-24'} flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300`}>
+    <header className={`${bg ? 'bg-tertiary h-20' : 'h-24'} flex items-center fixed top-0 w-full text-white z-20 transition-all duration-300`}>
       <div className='container mx-auto h-full flex items-center justify-between'>
         {/* logo */}
         <Link to="home" spy={true} smooth={true} duration={500} >
-        <h1 className='text-4xl leading-[44px] font-bold'>Ruggero</h1>
+          <h1 className='text-4xl leading-[44px] font-bold cursor-pointer'>Ruggero</h1>
         </Link>
         {/* nav */}
         <div className='hidden lg:block'>
